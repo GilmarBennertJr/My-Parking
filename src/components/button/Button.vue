b5fc9b21da7e9254baa126b6ec9e766e8a56332b
GilmarBennertJr <gilmarbennertjunior302@gmail.com>
2022-04-27 20:20:43 -0300

    [INIT] - Commit Inicial

---
+++ src/components/button/Button.vue
@@ -0,0 +1,45 @@
+<template>
+    <button :type="getTypeAction" class="btn" :class="getType" @click="disparaAcao()">
+        <i class="fas" :class="getIcon" style="margin-right: 5px" v-if="icon"></i>
+        <slot></slot>
+    </button>    
+</template>
+
+<script>
+    export default {
+        props: ['type', 'icon', 'submit'],
+        methods: {
+            disparaAcao() {
+                if (this.getTypeAction === 'button') {
+                    this.$emit('click', {teste: 'Teste'})
+                }
+            }
+        },
+        computed: {
+            getIcon() {
+                switch (this.icon) {
+                    case 'save': return 'fa-save'
+                    case 'delete': return 'fa-trash'
+                    default: return ''
+                }
+            },
+            getType() {
+                switch (this.type) {
+                    case 'primary': return 'btn-outline-primary'
+                    case 'danger': return 'btn-outline-danger'
+                    default: return 'btn-outline-dark';
+                }
+            },
+            getTypeAction() {
+                if (this.submit) {
+                    return 'submit'
+                }
+                return 'button';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container mt-3">
+        <div class="text-center">
+            <img src="../../assets/logo.png" class="width300px">
+        </div>
+        <hr>
+        <h4 v-text="title"></h4>
+        <hr>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['title']
+    }
+</script>
+<style>
+    .width300px {
+        width: 300px;
+    }
+</style>
